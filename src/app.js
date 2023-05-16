8ce6304f0ae4664159f1a4abf4f958d4534a825a
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2023-05-16 15:05:51 +0500

    shit fixed

--- src/app.js
+++ src/app.js
@@ -5,15 +5,15 @@ const app = express();
 
 app.get("/tests", async (req, res) => {
   try {
-    const testQuery = APIFeatures(Test.find(), req.query)
+    const testQuery = new APIFeatures(Test.find(), req.query)
       .sort()
       .filter()
       .paginate()
       .limitFields();
 
-    res.status(200).json({ status: "success", data: await testQuery });
+    res.status(200).json({ status: "success", data: await testQuery.query });
   } catch (error) {
-    res.status(404).json({ status: "error", error });
+    res.status(404).json({ status: "error", error: error.message });
   }
 });
 
