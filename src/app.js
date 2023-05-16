2fefd5eac79411bdb27b3fcdbcc520f3a2da624f
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2023-05-16 15:01:23 +0500

    shit fixed

--- src/app.js
+++ src/app.js
@@ -4,13 +4,17 @@ const Test = require("./modules/testModel");
 const app = express();
 
 app.get("/tests", async (req, res) => {
-  const testQuery = APIFeatures(Test.find(), req.query)
-    .sort()
-    .filter()
-    .paginate()
-    .limitFields();
+  try {
+    const testQuery = APIFeatures(Test.find(), req.query)
+      .sort()
+      .filter()
+      .paginate()
+      .limitFields();
 
-  res.status(200).json({ status: "success", data: await testQuery });
+    res.status(200).json({ status: "success", data: await testQuery });
+  } catch (error) {
+    res.status(404).json({ status: "error", error });
+  }
 });
 
 module.exports = app;
